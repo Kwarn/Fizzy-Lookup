25d9b1431fda4e588ab767de6919ea4e12b77625
Karl Warner <KarlWarner.dev@gmail.com>
2020-04-22 13:28:20 +0100

    Added title and icon

--- FizzyLookup.py
+++ FizzyLookup.py
@@ -7,6 +7,8 @@ import numpy as np
 class FizzyLookup(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Fizzy Lookup")
+        self.iconbitmap("FizzyLookupIcon.ico")
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
@@ -119,7 +121,7 @@ class StartPage(tk.Frame):
         """TOP FRAME"""
 
         top_frame = tk.Frame(self, bg='black')
-        column_grid_config(top_frame, 7)
+        column_grid_config(top_frame, 9)
         top_frame.grid(row=0, column=0, sticky="nsew")
 
         loc_label = ttk.Label(top_frame, text="Location", anchor="center")
