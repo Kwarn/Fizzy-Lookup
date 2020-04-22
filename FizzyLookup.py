e69ce38ba734c1f1433aeea276ffe4886cd0eccb
Karl Warner <KarlWarner.dev@gmail.com>
2020-04-22 13:13:20 +0100

    Added search by floor functionality, Updated test sample excel sheet

--- FizzyLookup.py
+++ FizzyLookup.py
@@ -31,8 +31,8 @@ class StartPage(tk.Frame):
 
         def convert_df_datetime_to_str(df):
             """Makes datetime more easily searchable"""
-            df['Available from'] = pd.to_datetime(df["Available from"], errors="coerce")
-            df['Available from'] = df['Available from'].dt.strftime('%m - %B - %Y')
+            df['Available'] = pd.to_datetime(df["Available"], errors="coerce")
+            df['Available'] = df['Available'].dt.strftime('%m - %B - %Y')
             return df
 
         def destroy_widgets(frame_to_clear):
@@ -52,22 +52,25 @@ class StartPage(tk.Frame):
             price_dict = {"£1000-£1499": [1000, 1499], "£1500-£1799": [1500, 1799], "£1800+": [1800, 10000]}
 
             location = loc_dict[loc_var.get()] if loc_var.get() != "Any" else 0
+            floor = int(floor_var.get()) if floor_var.get() != "Any" else 0
             bedroom = int(bed_var.get()) if bed_var.get() != "Any" else 0
             bathroom = int(bath_var.get()) if bath_var.get() != "Any" else 0
             furnished = fur_dict[fur_var.get()] if fur_var.get() != "Any" else 0
             price = price_dict[price_var.get()] if price_var.get() != "Any" else 0
             available = avail_var.get() if avail_var.get() != "Any" else 0
 
-            if not any([location] + [bedroom] + [bathroom] + [furnished] + [price] + [available]):
+            if not any([location] + [floor] + [bedroom] + [bathroom] + [furnished] + [price] + [available]):
                 create_labels_from_df(pd.DataFrame(), True)
             else:
-                create_search_str(location, bedroom, bathroom, furnished, price, available)
+                create_search_str(location, floor, bedroom, bathroom, furnished, price, available)
 
-        def create_search_str(location, bedroom, bathroom, furnished, price, available):
+        def create_search_str(location, floor, bedroom, bathroom, furnished, price, available):
             """Uses eval() to search df with joined string"""
             search = []
             if location:
-                search.append("(df['Unique code'].str.contains(location))")
+                search.append("(df['Location'].str.contains(location))")
+            if floor:
+                search.append("(df['Floor'] == floor)")
             if bedroom:
                 search.append("(df['Bedroom'] == bedroom)")
             if bathroom:
@@ -77,7 +80,7 @@ class StartPage(tk.Frame):
             if price:
                 search.append("((df['Price'] >= price[0]) & (df['Price'] <= price[1]))")
             if available:
-                search.append("((df['Available from'].str.contains(available)) & (df['Available from'] != 'NaN'))")
+                search.append("((df['Available'].str.contains(available)) & (df['Available'] != 'NaN'))")
 
             df = convert_df_datetime_to_str(get_df_from_excel())
             destroy_widgets(scrollable_frame)
@@ -87,20 +90,21 @@ class StartPage(tk.Frame):
             if load_all:
                 df = convert_df_datetime_to_str(get_df_from_excel())
 
-            headers = [ttk.Label(scrollable_frame, text=label) for label in df.columns[:-1]]
-            unique_code = [ttk.Label(scrollable_frame, text=row["Unique code"]) for index, row in df.iterrows()]
+            headers = [ttk.Label(scrollable_frame, text=label) for label in df.columns[:10]]
+            location = [ttk.Label(scrollable_frame, text=row["Location"]) for index, row in df.iterrows()]
             flat_num = [ttk.Label(scrollable_frame, text=row["Flat #"]) for index, row in df.iterrows()]
+            floor = [ttk.Label(scrollable_frame, text=row["Floor"]) for index, row in df.iterrows()]
             bedroom = [ttk.Label(scrollable_frame, text=row["Bedroom"]) for index, row in df.iterrows()]
             sqft = [ttk.Label(scrollable_frame, text=row["Sq Ft"]) for index, row in df.iterrows()]
             bathroom = [ttk.Label(scrollable_frame, text=row["Bathroom"]) for index, row in df.iterrows()]
             fur_unf = [ttk.Label(scrollable_frame, text=row["Fur/Unf"]) for index, row in df.iterrows()]
             price = [ttk.Label(scrollable_frame, text=row["Price"]) for index, row in df.iterrows()]
             balcony = [ttk.Label(scrollable_frame, text=row["Balcony"]) for index, row in df.iterrows()]
-            available_from = [ttk.Label(scrollable_frame, text=row["Available from"]) for index, row in df.iterrows()]
-            data_labels = [unique_code, flat_num, bedroom, sqft, bathroom, fur_unf, price, balcony, available_from]
+            available = [ttk.Label(scrollable_frame, text=row["Available"]) for index, row in df.iterrows()]
+            data_labels = [location, flat_num, floor, bedroom, sqft, bathroom, fur_unf, price, balcony, available]
 
             grid_labels(headers, 0, True)
-            [grid_labels(data_labels[i], i) for i in range(len(data_labels))]
+            [grid_labels(data_labels[i], i, False) for i in range(len(data_labels))]
 
         def grid_labels(labels, col, is_first_row=False):
             if is_first_row:
@@ -120,16 +124,18 @@ class StartPage(tk.Frame):
 
         loc_label = ttk.Label(top_frame, text="Location", anchor="center")
         loc_label.grid(row=0, column=0, sticky="nsew")
+        floor_label = ttk.Label(top_frame, text="Floor", anchor="center")
+        floor_label.grid(row=0, column=1, sticky="nsew")
         bed_label = ttk.Label(top_frame, text="Bedrooms", anchor="center")
-        bed_label.grid(row=0, column=1, sticky="nsew")
+        bed_label.grid(row=0, column=2, sticky="nsew")
         bath_label = ttk.Label(top_frame, text="Bathrooms", anchor="center")
-        bath_label.grid(row=0, column=2, sticky="nsew")
+        bath_label.grid(row=0, column=3, sticky="nsew")
         fur_label = ttk.Label(top_frame, text="Furnished", anchor="center")
-        fur_label.grid(row=0, column=3, sticky="nsew")
+        fur_label.grid(row=0, column=4, sticky="nsew")
         price_label = ttk.Label(top_frame, text="Price", anchor="center")
-        price_label.grid(row=0, column=4, sticky="nsew")
+        price_label.grid(row=0, column=5, sticky="nsew")
         avail_label = ttk.Label(top_frame, text="Available", anchor="center")
-        avail_label.grid(row=0, column=5, sticky="nsew")
+        avail_label.grid(row=0, column=6, sticky="nsew")
 
         loc_var = tk.StringVar(top_frame)
         loc_var.set("Any")
@@ -137,34 +143,39 @@ class StartPage(tk.Frame):
                                    "Lewisham", "Walthamstow", "Hayes", "Stepney Green")
         loc_option.grid(row=1, column=0, sticky="nsew")
 
+        floor_var = tk.StringVar(top_frame)
+        floor_var.set("Any")
+        floor_options = tk.OptionMenu(top_frame, floor_var, "Any", "1", "2", "3", "4", "5")
+        floor_options.grid(row=1, column=1, sticky="nsew")
+
         bed_var = tk.StringVar(top_frame)
         bed_var.set("Any")
         bed_option = tk.OptionMenu(top_frame, bed_var, "Any", "1", "2", "3")
-        bed_option.grid(row=1, column=1, sticky="nsew")
+        bed_option.grid(row=1, column=2, sticky="nsew")
 
         bath_var = tk.StringVar(top_frame)
         bath_var.set("Any")
         bath_option = tk.OptionMenu(top_frame, bath_var, "Any", "1", "2", "3")
-        bath_option.grid(row=1, column=2, sticky="nsew")
+        bath_option.grid(row=1, column=3, sticky="nsew")
 
         fur_var = tk.StringVar(top_frame)
         fur_var.set("Any")
         fur_option = tk.OptionMenu(top_frame, fur_var, "Any", "Furnished", "Unfurnished")
-        fur_option.grid(row=1, column=3, sticky="nsew")
+        fur_option.grid(row=1, column=4, sticky="nsew")
 
         price_var = tk.StringVar(top_frame)
         price_var.set("Any")
         price_option = tk.OptionMenu(top_frame, price_var, "Any", "£1000-£1499", "£1500-£1799", "£1800+")
-        price_option.grid(row=1, column=4, sticky="nsew")
+        price_option.grid(row=1, column=5, sticky="nsew")
 
         avail_var = tk.StringVar(top_frame)
         avail_var.set("Any")
         avail_option = tk.OptionMenu(top_frame, avail_var, "Any", "January", "Febuary", "March", "April", "May", "June",
                                      "July", "August", "September", "October", "November", "December")
-        avail_option.grid(row=1, column=5, sticky="nsew")
+        avail_option.grid(row=1, column=6, sticky="nsew")
 
         search_button = tk.Button(top_frame, text="Search", command=lambda: set_search_options())
-        search_button.grid(row=0, column=6, rowspan=2, columnspan=2, sticky="nsew")
+        search_button.grid(row=0, column=7, rowspan=2, columnspan=2, sticky="nsew")
 
         """CENTER FRAME"""
 
@@ -178,7 +189,7 @@ class StartPage(tk.Frame):
 
         scrollable_frame = tk.Frame(canvas)
         scrollable_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
-        column_grid_config(scrollable_frame, 8)
+        column_grid_config(scrollable_frame, 9)
 
         canvas.bind_all('<MouseWheel>', lambda event: canvas.yview_scroll(int(-1*(event.delta/120)), "units"))
         canvas.configure(yscrollcommand=scrollbar.set)
